49d238943b097518daaa099fddd0a41a513a3104
Thanusan Aiyadurai Iyer <thanu@mbpvonthanusan-1.home>
2023-12-22 15:00:59 +0100

    Neustes commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,247 @@
+import React from "react";
+import { Link as LinkR } from "react-router-dom";
+import styled from "styled-components";
+import { FiCodesandbox } from "react-icons/fi";
+import { FaBars } from "react-icons/fa";
+import { useTheme } from "styled-components";
+import { Bio } from "../../data/constants";
+
+const Nav = styled.div`
+    background-color: ${({theme}) => theme.card_light};
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1rem;
+    position: sticky;
+    top: 0;
+    z-index: 10;
+    @media screen and (max-width: 960px) {
+        transtion: 0.8s all ease;
+    }
+`;
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1200px;
+`;
+
+const NavLogo = styled(LinkR)`
+    width: 80%;    
+    padding: 0 6px;
+    display: flex;
+    justify-self: flex-start;
+    cursor: pointer;
+    align-items: center;
+    text-decoration: none;
+    @media screen and (max-width: 540px) {
+      padding: 0 0px;
+  }
+`;
+const Span = styled.div`
+    padding: 0 4px;
+    font-weight: bold;
+    font-size: 18px;
+`;
+const NavItems = styled.ul`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 32px;
+    list-style: none;
+
+    @media screen and (max-width: 768px) {
+      display: none;
+    }
+`;
+
+const NavLink = styled.a`
+    color: ${({ theme }) => theme.text_primary};
+    font-weight: 500;
+    cursor: pointer;
+    text-decoration: none;
+    transition: all 0.2s ease-in-out;
+    :hover {
+      color: ${({ theme }) => theme.primary};
+    }
+`;
+
+const GitHubButton = styled.a`
+  background-color: transparent;
+  color: ${({ theme }) => theme.primary};
+  border: 1.8px solid ${({ theme }) => theme.primary};
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px;
+  font-size: 1rem;
+  font-weight: 500;
+  cursor: pointer;
+  height: 70%;
+    :hover {
+      background-color: ${({ theme }) => theme.primary};
+      color: ${({ theme }) => theme.white};     
+    }
+    @media screen and (max-width: 640px) { 
+    font-size: 0.8rem;
+    }
+`;
+
+const ButtonContainer = styled.div`
+  width: 80%;  
+  height: 100%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  padding: 0 6px;
+  @media screen and (max-width: 640px) {
+    display: none;
+  }
+`;
+
+const MobileIcon = styled.div`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 50%);
+    font-size: 1.5rem;
+    cursor: pointer;
+    color: ${({ theme }) => theme.text_primary};
+  }
+`;
+
+const MobileMenu = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 16px;
+    position: absolute;
+    top: 80px;
+    right: 0;
+    width: 100%;
+    padding: 12px 40px 24px 40px;
+    background: ${({ theme }) => theme.card_light};
+    transition: all 0.3s ease-in-out;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    border-radius: 0 0 20px 20px;
+    box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.3);
+    opacity: ${({ open }) => (open ? '1' : '0')};
+    z-index: ${({ open }) => (open ? '1' : '-1')};
+
+`;
+
+const MobileMenuLinks = styled(LinkR)`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+
+const Navbar = () => {
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    return (
+      <Nav>
+        <NavContainer>
+          <NavLogo to="/">
+              <a
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  color: "white",
+                  marginBottom: '20;',
+                  cursor: 'pointer' 
+                }}
+              >    
+                <FiCodesandbox size="3rem" /> <Span>Portfolio Thanusan</Span>
+              </a>
+          </NavLogo>
+          <MobileIcon>
+              <FaBars 
+                onClick={() => {
+                  setOpen(!open);
+                }} 
+              /> 
+          </MobileIcon>
+          <NavItems>
+              <NavLink href="#about">Home</NavLink>
+              <NavLink href="#skills">Fähigkeiten</NavLink>
+              <NavLink href="#experience">Arbeitserfahrung</NavLink>
+              <NavLink href="#education">Bildungsweg</NavLink>
+          </NavItems>
+          <ButtonContainer>
+            <GitHubButton href={Bio.github} target="_blank">Github Profil</GitHubButton>
+          </ButtonContainer>
+        </NavContainer>
+        {
+          open && (
+          <MobileMenu open={open}>
+            <MobileMenuLinks 
+              href="#about"
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              Home
+            </MobileMenuLinks>
+
+            <MobileMenuLinks
+              href="#skills"
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              Fähigkeiten
+            </MobileMenuLinks>
+
+            <MobileMenuLinks
+              href='#experience'
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              Arbeitserfahrung
+            </MobileMenuLinks>
+
+            <MobileMenuLinks
+              href='#education'
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              Bildungsweg
+            </MobileMenuLinks>
+
+            <GitHubButton
+                style={{
+                padding: '10px 16px',
+                background: `${theme.primary}`,
+                color: 'white',width: 'max-content'
+                }} 
+                href={Bio.github} target="_blank"
+            >
+                Github Profil
+            </GitHubButton>
+          </MobileMenu>
+          )
+        };
+      </Nav>
+    );
+};
+
+export default Navbar;
+
---
+++ src/data/constants.js
@@ -0,0 +1,136 @@
+export const Bio = {
+    name: "Thanusan Aiyadurai Iyer",
+    roles: [
+      "Student",
+      "Customer Care Advisor",
+      "Wirtschaftsinformatiker",
+    ],
+    description:
+      "Ich bin eine engagierte, anpassungsfähige Person, die ständig bereit ist, neue Aufgaben zu übernehmen. Ich habe einen starken Wunsch zu lernen und bin bestrebt, hervorragende Arbeit zu leisten. Ich bin bereit, einen bedeutenden Beitrag zu leisten und Großes zu erreichen, weil ich eine wachstumsorientierte Denkweise und einen optimistischen Ausblick habe.",
+    github: "https://github.com/Thanu221099",    
+    linkedin: "https://www.linkedin.com/in/thanusan-aiyadurai-iyer/",
+};
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },        
+        {
+          name: "Figma",
+          image:
+            "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/de/archive/2/24/20230216090451%21Logo_Swisscom.svg",
+      role: "Customer Care Advisor",
+      company: "Swisscom AG",
+      date: "Aug 2019 - Jetzt",
+      desc: "Ich arbeite momentan im Netzbau als Customer Care Advisor",
+      activities: [
+        "Kundenbetreuung",
+
+        "Bearbeitung von Netzanfragen",
+
+        "Erfassung von Netzerschliessungsanfragen",
+      ],
+    },
+    {
+      id: 1,
+      img: "https://upload.wikimedia.org/wikipedia/de/archive/2/24/20230216090451%21Logo_Swisscom.svg",
+      role: "Kaufmann EFZ",
+      company: "Swisscom AG",
+      date: "Aug 2016 - Jul 2019",
+      desc: "Ich habe meine Ausbildung als Kaufmann EFZ bei der Swisscom AG absolviert.",
+      activities: [
+        "Bearbeitung von Kooperationsanfragen ",
+
+        "Bearbeitung von Kreditoren",
+
+        "Beratung im 1st Level Support",
+
+        "Kundenberatung im Swisscom Shop",
+
+        "Rekrutierung von Lernenden",
+
+        "Assistent in verschiedenen Events",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://upload.wikimedia.org/wikipedia/commons/a/a2/BFH_Logo_deutsch.png",
+      school: "Berner Fachhochschule",
+      date: "Sep 2021 - Jetzt",
+      desc: "Aktuell studiere ich an der BFH den Studiengang Wirtschaftsinformatik.",
+      degree: "Bachelor of Science - Wirtschaftsinformatik",
+    },
+    {
+      id: 1,
+      img: "https://www.wksbern.ch/sites/default/files/2021-11/wks_logo.svg",
+      school: "WKS KV Bildung",
+      date: "Aug 2019 - Jul 2021",
+      desc: "Ich absolvierte meine Berufsmatura bei der WKS KV Bildung.",
+      degree: "Berufsmaturität",
+    },
+    {
+      id: 2,
+      img: "https://www.wksbern.ch/sites/default/files/2021-11/wks_logo.svg",
+      school: "WKS KV Bildung",
+      date: "Aug 2016 - Jul 2019",
+      desc: "Ich ging für meine Ausbildung bei der WKS KV Bildung in die Berufsschule.",
+      degree: "Kaufmann EFZ",
+    },
+  ];
+  
